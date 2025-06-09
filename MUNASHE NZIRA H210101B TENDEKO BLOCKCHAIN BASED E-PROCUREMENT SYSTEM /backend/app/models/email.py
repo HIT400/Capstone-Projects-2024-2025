a6b2feacebe681080d1cd936ffc0de755d2a6bda
Brendon <H210371H@hit.ac.zw>
2025-06-09 18:22:09 +0200

    Added zip folder for Brendon Tsveta H210371H

--- MUNASHE NZIRA H210101B TENDEKO BLOCKCHAIN BASED E-PROCUREMENT SYSTEM /backend/app/models/email.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class EmailSchema(BaseModel):
-    email: EmailStr
-    subject: str
-    body: str
--- MUNASHE NZIRA H210101B TENDEKO BLOCKCHAIN BASED E-PROCUREMENT SYSTEM /smart_contract/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const TendekoEProcurement = artifacts.require("TendekoEProcurement");
-const DataVerification = artifacts.require("DataVerification");
-
-module.exports = async function (deployer, network, accounts) {
-  const adminAddress = accounts[0];
-
-  await deployer.deploy(TendekoEProcurement, adminAddress);
-  await deployer.deploy(DataVerification);
-};
